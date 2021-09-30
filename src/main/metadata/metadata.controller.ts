a568a6fd6c7dc900e8600372b7cf373651697bb9
Cyan Kinesin <4thston@gmail.com>
2021-09-30 14:39:05 +0900

    Apply metadata service to controller

--- src/main/metadata/metadata.controller.ts
+++ src/main/metadata/metadata.controller.ts
@@ -1,22 +1,27 @@
 import { Router } from 'express';
+import { Metadata } from 'main/models/metadata.model';
 import { ControllerInterface } from '../controller.interface';
+import { MetadataService } from './metadata.service';
 
 
 export class MetadataController implements ControllerInterface {
 
     router: Router = Router();
 
+    service = new MetadataService();
+
     constructor() {
 
-        this.router.get('/', (req, res) => {
-            res.send({
-                message: 'get called'
-            });
+        this.router.get('/', async (req, res) => {
+            const allMetadata = await this.service.getAllMetadatas();
+            res.send(allMetadata);
         });
-        this.router.post('/', (req, res) => {
-            res.send({
-                message: 'post called'
-            });
+
+        this.router.post('/', async (req, res) => {
+
+            const newMetdata = req.body as Metadata;
+            const created = await this.service.saveMetadatas(newMetdata);
+            res.send(created);
         });
     }
 
