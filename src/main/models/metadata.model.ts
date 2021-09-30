4f301eab8bcce3c0f7989cfb616b685bb3c83a5a
Cyan Kinesin <4thston@gmail.com>
2021-09-30 15:54:37 +0900

    Complement metadata model

--- src/main/models/metadata.model.ts
+++ src/main/models/metadata.model.ts
@@ -4,7 +4,7 @@
  * @param date DateStringFormat 2020-10-20T17:34:36.000Z
  * @param description Metadata description: Project templates, IntelliSense, npm integration, debugging, & more. Turn Visual Studio into a powerful Node.js development environment. Download for free.
  * @param image image url: https://visualstudio.microsoft.com/wp-content/uploads/2018/06/vscom_vs_features_node-js_twitter.png
- * @param publisher publisher: Visual Studio
+ * @param publisher publisher: og:site_name -> Visual Studio
  * @param title html title. Node.js Developer Tools | Visual Studio
  * @param url source url. https://visualstudio.microsoft.com/ko/vs/features/node-js/
  */
