a210cc85013c2799926d0a111ddde56de6daecfa
Cyan Kinesin <4thston@gmail.com>
2021-09-30 16:35:52 +0900

    Implement all metadata fetching from db

--- src/main/metadata/metadata.dao.ts
+++ src/main/metadata/metadata.dao.ts
@@ -1,9 +1,15 @@
+import { model } from 'mongoose';
+
 import { Metadata } from 'main/models/metadata.model';
 
 export class MetadataDao {
 
-    async getMetadatas(): Promise<Metadata[]> {
-        return [];
+    async getAllMetadatas(): Promise<Metadata[]> {
+
+        const Metadata = model('Metadata');
+        const allMetadata = await Metadata.find() as any as Metadata[];
+
+        return allMetadata;
     }
 
     async saveMetadatas(newMetadata: Metadata): Promise<Metadata> {
